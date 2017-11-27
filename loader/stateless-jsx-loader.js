adb38f31c6807bd29ed212634402a44c9b3867d7
Mircea M <mircea.moise@gmail.com>
2017-11-27 17:08:25 +0000

    Fix name to be as per js standards

--- loader/stateless-jsx-loader.js
+++ loader/stateless-jsx-loader.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 
 module.exports = function (content) {
-    let class_template = fs.readFileSync('./loader/component_template.jsx', 'utf-8');
+    let class_template = fs.readFileSync('./loader/component-template.jsx', 'utf-8');
 
     return class_template.replace("$$jsx_content$$", content).replace("$$class_name$$", "Hello");
 };
