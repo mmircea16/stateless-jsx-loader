6c76dc5e2106d3741cb973b280d4391dc0133189
Mircea M <mircea.moise@gmail.com>
2018-01-02 16:59:11 +0000

    Added dependency are per recommendation here: https://webpack.js.org/contribute/writing-a-loader/

--- loader/stateless-jsx-loader.js
+++ loader/stateless-jsx-loader.js
@@ -3,9 +3,13 @@ const path = require('path');
 const loaderUtils = require('loader-utils');
 
 module.exports = function (content) {
-    let classTemplate = fs.readFileSync(path.resolve(__dirname, 'component-template.jsx'), 'utf-8');
+    let pathToTemplate = path.resolve(__dirname, 'component-template.jsx');
+    this.addDependency(pathToTemplate);
+
+    let classTemplate = fs.readFileSync(pathToTemplate, 'utf-8');
 
     let name = loaderUtils.interpolateName(this, "[name]", {});
     name = name.replace('.html', '');
+
     return classTemplate.replace("$$jsx_content$$", content).replace("$$class_name$$", name);
 };
