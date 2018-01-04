2e003f52372d19c1f6cddf52c60566bbb6a9ce26
Mircea M <mircea.moise@gmail.com>
2018-01-04 16:13:30 +0000

    Small refactoring

--- src/stateless-jsx-loader.js
+++ src/stateless-jsx-loader.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
-const fillTemplate = require('./jsx-stateless')
-const getFileName = require('./utils')
+const fillTemplate = require('./jsx-stateless');
+const getFileName = require('./utils');
 const path = require('path');
 
 module.exports = function (content) {
     let pathToTemplate = path.resolve(__dirname, 'component-template.jsx');
     this.addDependency(pathToTemplate);
 
-    let classTemplate = fs.readFileSync(pathToTemplate, 'utf-8');
+    let template = fs.readFileSync(pathToTemplate, 'utf-8');
 
     let className = getFileName(this);
 
-    return fillTemplate(classTemplate, className, content);
+    return fillTemplate(template, className, content);
 };
